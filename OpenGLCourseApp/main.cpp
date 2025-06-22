cafe0efca2bbed018a4caac924fd5c8bc074d05d
Brendan Cook <brendancook44@gmail.com>
2025-06-21 22:39:56 -0500

    Remove animation code

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -19,18 +19,6 @@ std::vector<Shader*> shaderList;
 
 GLuint uniformModel, uniformProjection;
 
-bool direction = true;
-float triOffset = 0.0f;
-float triMaxOffset = 0.7f;
-float triIncrement = 0.005f;
-
-float currentAngle = 0.0f;
-
-float sizeDirection = true;
-float currentSize = 0.4f;
-float maxSize = 0.8f;
-float minSize = 0.1f;
-
 static const char* fShader = "Shaders/shader.fragment";
 static const char* vShader = "Shaders/shader.vertex";
 
@@ -86,46 +74,6 @@ int main()
 		// Get + Handle User Input Events
 		glfwPollEvents();
 
-		// Update Triangle Offset
-		// Change Direction if Offset Exceeds Max Offset
-		if (direction)
-		{
-			triOffset += triIncrement;
-		}
-		else
-		{
-			triOffset -= triIncrement;
-		}
-
-		if (abs(triOffset) >= triMaxOffset)
-		{
-			direction = !direction;
-		}
-
-		// Update Current Angle
-		// Increment by a Small Value and Wrap Around at 360 Degrees
-		currentAngle += 0.1f;
-		if (currentAngle >= 360.0f)
-		{
-			currentAngle -= 360.0f;
-		}
-
-		// Update Triangle Size
-		// Change Direction if Size Exceeds Max Size
-		if (sizeDirection)
-		{
-			currentSize += 0.001f;
-		}
-		else
-		{
-			currentSize -= 0.001f;
-		}
-
-		if (currentSize >= maxSize || currentSize <= minSize)
-		{
-			sizeDirection = !sizeDirection;
-		}
-
 		// Clear Window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -137,14 +85,14 @@ int main()
 		// Create Model Matrix
 		glm::mat4 model(1.0f);
 
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, -2.5f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		meshList[0]->RenderMesh();
 
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(-triOffset, 1.0f, -2.5f));
+		model = glm::translate(model, glm::vec3(0.0f, 1.0f, -2.5f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		meshList[1]->RenderMesh();

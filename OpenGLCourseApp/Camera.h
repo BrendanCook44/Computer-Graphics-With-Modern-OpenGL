4e65afad6fd2e28a31e2543eb3593545bee173ed
Brendan Cook <brendancook44@gmail.com>
2025-07-12 22:08:04 -0500

    camera functions

--- OpenGLCourseApp/Camera.h
+++ OpenGLCourseApp/Camera.h
@@ -4,10 +4,17 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <GLFW/glfw3.h>
+
 class Camera
 {
 	public:
 		Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
+
+		void keyControl(bool* keys);
+
+		glm::mat4 calculateViewMatrix();
+
 		~Camera();
 
 	private:
@@ -20,7 +27,7 @@ class Camera
 		GLfloat yaw;
 		GLfloat pitch;
 
-		GLfloat movementSpeed;
+		GLfloat moveSpeed;
 		GLfloat turnSpeed;
 
 		void update();

ea2754f1a7a94bd919210d22d7bfaa3f58a6062a
Brendan Cook <brendancook44@gmail.com>
2025-07-23 21:05:21 -0500

    Add plain texture

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -162,7 +162,7 @@ int main()
 	unsigned int pointLightCount = 0;
 
 	pointLights[0] = PointLight(0.0f, 0.0f, 1.0f,
-								0.1f, 0.4f,
+								0.1f, 1.0f,
 								4.0f, 0.0f, 0.0f,
 								0.3f, 0.2f, 0.1f);
 

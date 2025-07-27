c3051b2e66c8d0953fe86a5a5fa242d60f6704c3
Brendan Cook <brendancook44@gmail.com>
2025-07-26 22:28:56 -0500

    Scale blackhawk

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -270,7 +270,6 @@ int main()
 
 		meshList[0]->RenderMesh();
 
-
 		// Pyramid 2
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, 4.0f, -2.5f));
@@ -285,7 +284,6 @@ int main()
 		// Floor
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, -2.0f, -0.0f));
-		// model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		dirtTexture.UseTexture();
 		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
@@ -305,7 +303,8 @@ int main()
 		// Blackhawk Model
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(-2.0f, 0.5f, 0.0f));
-		model = glm::scale(model, glm::vec3(0.050f, 0.050f, 0.050f));
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);

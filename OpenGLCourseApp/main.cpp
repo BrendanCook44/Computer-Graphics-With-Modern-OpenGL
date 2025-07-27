94576fd2bfcf66159102c7788c3ec8e40e8e0083
Brendan Cook <brendancook44@gmail.com>
2025-07-26 22:36:53 -0500

    blackhawk position fixes

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -304,6 +304,7 @@ int main()
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(-2.0f, 0.5f, 0.0f));
 		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(-180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
